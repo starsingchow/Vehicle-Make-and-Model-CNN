5c4eff55bc79bfd5501b91e29e3d8582151c8fe3
starsingchow <starstarstarchow@gmail.com>
2018-03-12 13:44:09 +0800

    data augmentation change

--- data_augmentation.py
+++ data_augmentation.py
@@ -11,11 +11,12 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Data Augmentation.')
 parser.add_argument('--dir', type = str, default='', help = 'input_data')
-
+parser.add_argument('--save_dir', type = str, default='', help ='save path')
 args = parser.parse_args(sys.argv[1:])
 DIR = args.dir
+SAVE = args.save_dir
 
-def data_augmenttation(dir, car_data):
+def data_augmenttation(dir, car_data, save):
     withPath = lambda file: '{}/{}'.format(dir,file)
     files = os.listdir(dir)
     images = dict((file, [cv2.imread(withPath(file)), car_data.loc[car_data['relative_im_path'] == file, 
@@ -39,96 +40,146 @@ def data_augmenttation(dir, car_data):
 
             ratio_results = []
             for result in results:
-                ratio_images = RandomRatio(result, 2)
+                ratio_images = RandomRatio(result, 1)
                 ratio_results += ratio_images
             results +=  ratio_results
 
             print('finsh ratio')
-            brightness_results = []
-            for result in results:
-                brightness = Brightness(result, 2)
-                brightness_results += brightness
-            results += brightness_results
+            # brightness_results = []
+            # for result in results:
+            #     brightness = Brightness(result, 2)
+            #     brightness_results += brightness
+            # results += brightness_results
 
-            print('finsh brightness')
+            # print('finsh brightness')
+
+            # colorJittering_results = []
+            # for result in results:
+            #     colorJittering = ColorJittering(result, 3)
+            #     colorJittering_results += colorJittering
+            # results += colorJittering_results
+            # print('finsh colorJittering')
+
             cropped_images = []
             for result in results:
-                cropped = RandomCrop(result, 3)
+                cropped = RandomCrop(result, 2)
                 cropped_images += cropped
             
             print('finsh crop')
             random.seed(12345)
-            EdgeEnhance_number = random.sample(range(len(cropped_images)), 15)
-            save_number = random.sample(range(len(cropped_images)), 15)
+            try:
+                save_number = random.sample(range(len(cropped_images)), 6)
+                EdgeEnhance_number = random.sample(range(len(cropped_images)), 2)
+                colorJittering_number = random.sample(range(len(cropped_images)), 3)
+                brightness_number = random.sample(range(len(cropped_images)), 3) 
+            except ValueError:
+                save_number = []
+                EdgeEnhance_number = random.sample(range(len(cropped_images)), 1)
+                colorJittering_number = random.sample(range(len(cropped_images)), 1)
+                brightness_number = random.sample(range(len(cropped_images)), 1) 
+            
             i = 0
+            if not os.path.exists(save+'/test'):
+                os.makedirs(save+'/test')
+            
+            if not os.path.exists(save+'/train'):
+                os.makedirs(save+'/train')
+
             for cropped_image in cropped_images:
                 if i not in save_number:
                     i +=1
                     continue
-                if i in EdgeEnhance_number:
+                if i in brightness_number:
+                    cropped_image = Brightness(cropped_image)
+
+                if i in colorJittering_number:
+                    cropped_image = ColorJittering(cropped_image)
+
+                if i in EdgeEnhance_number and i not in colorJittering_number:
                     cropped_image = EdgeEnhance(cropped_image)
 
                 cropped_image = cv2.cvtColor(cropped_image, cv2.COLOR_RGB2BGR)
-                save_name = dir + '/{0}_'.format(i) + key
+                name_label = car_data.loc[car_data['relative_im_path'] == key, ['class', 'test']]
+                save_type = 'train'
+                if int(name_label['test']) == 1:
+                    save_type = 'test'
+                save_name = save + '{0}/Num{1}_label_{2}_'.format(save_type,i,int(name_label['class'])) + key
                 print(save_name)
                 cv2.imwrite(save_name, cropped_image)
                 i += 1
             
 def bbox_crop(image, bbox_crop):
     image_shape = image.shape
-    if image_shape[0] < 224 and image_shape[1] < 224:
-        return image
-    elif image_shape[1] < 224:
-        y1 = bbox_crop['bbox_y1'].get_values()[0] - 16
-        if y1 < 0:
-            y1 = 0
+    # if image_shape[0] < 224 and image_shape[1] < 224:
+    #     return image
+    # elif image_shape[1] < 224:
+    #     y1 = bbox_crop['bbox_y1'].get_values()[0] - 16
+    #     if y1 < 0:
+    #         y1 = 0
 
-        y2 = bbox_crop['bbox_y2'].get_values()[0] + 16
-        if y2 > image_shape[0]:
-            y2 = image_shape[0]
-        return image[y1:y2, :]
-    elif image_shape[0] < 224:
-        x1 = bbox_crop['bbox_x1'].get_values()[0] - 16
-        if x1 < 0:
-            x1 = 0
+    #     y2 = bbox_crop['bbox_y2'].get_values()[0] + 16
+    #     if y2 > image_shape[0]:
+    #         y2 = image_shape[0]
+    #     return image[y1:y2, :]
+    # elif image_shape[0] < 224:
+    #     x1 = bbox_crop['bbox_x1'].get_values()[0] - 16
+    #     if x1 < 0:
+    #         x1 = 0
 
-        x2 = bbox_crop['bbox_x2'].get_values()[0] + 16
-        if x2 > image_shape[1]:
-            x2 = image_shape[1]
-        return image[:, x1:x2]
-    else:
-        x1 = bbox_crop['bbox_x1'].get_values()[0] - 16
-        if x1 < 0:
-            x1 = 0
+    #     x2 = bbox_crop['bbox_x2'].get_values()[0] + 16
+    #     if x2 > image_shape[1]:
+    #         x2 = image_shape[1]
+    #     return image[:, x1:x2]
+    # else:
+    #     x1 = bbox_crop['bbox_x1'].get_values()[0] - 16
+    #     if x1 < 0:
+    #         x1 = 0
 
-        x2 = bbox_crop['bbox_x2'].get_values()[0] + 16
-        if x2 > image_shape[1]:
-            x2 = image_shape[1]
+    #     x2 = bbox_crop['bbox_x2'].get_values()[0] + 16
+    #     if x2 > image_shape[1]:
+    #         x2 = image_shape[1]
 
-        y1 = bbox_crop['bbox_y1'].get_values()[0] - 16
-        if y1 < 0:
-            y1 = 0
+    #     y1 = bbox_crop['bbox_y1'].get_values()[0] - 16
+    #     if y1 < 0:
+    #         y1 = 0
 
-        y2 = bbox_crop['bbox_y2'].get_values()[0] + 16
-        if y2 > image_shape[0]:
-            y2 = image_shape[0]
-        return image[y1:y2, x1:x2, :]
+    #     y2 = bbox_crop['bbox_y2'].get_values()[0] + 16
+    #     if y2 > image_shape[0]:
+    #         y2 = image_shape[0]
+    #     return image[y1:y2, x1:x2, :]
+
+    x1 = bbox_crop['bbox_x1'].get_values()[0] - 16
+    if x1 < 0:
+        x1 = 0
+
+    x2 = bbox_crop['bbox_x2'].get_values()[0] + 16
+    if x2 > image_shape[1]:
+        x2 = image_shape[1]
+
+    y1 = bbox_crop['bbox_y1'].get_values()[0] - 16
+    if y1 < 0:
+        y1 = 0
+
+    y2 = bbox_crop['bbox_y2'].get_values()[0] + 16
+    if y2 > image_shape[0]:
+        y2 = image_shape[0]
+    return image[y1:y2, x1:x2, :]
 
 def Squash(image):
     image_shape = image.shape
-    if image_shape[0] < 256 and image_shape[1] < 256:
-        return image
+    # if image_shape[0] < 256 and image_shape[1] < 256:
+    #     return image
     
-    if image_shape[0] >=  image_shape[1]:
-        ratio = 256 / image_shape[0]
-    else:
-        ratio = 256 /image_shape[1]
+    # if image_shape[0] >=  image_shape[1]:
+    #     ratio = 256 / image_shape[0]
+    # else:
+    #     ratio = 256 /image_shape[1]
     
     image.astype('Float64')
     
-    x = int(image_shape[0] * ratio)
-    y = int(image_shape[1] * ratio)
-    resized = tf.image.resize_images(image, [x, y], method = 0)
+    # x = int(image_shape[0] * ratio)
+    # y = int(image_shape[1] * ratio)
+    resized = tf.image.resize_images(image, [256, 256], method = 0)
     return resized.eval()
 
 def RandomCrop(image, numbers):
@@ -161,30 +212,45 @@ def RandomFlip(image):
     flipped_image = tf.image.flip_left_right(image)
     return flipped_image.eval()
 
-def ColorJittering(image, numbers):
-    h, w, c = image.shape
-    noise_images = []
-    for i in range(numbers):
-        noise = np.random.randint(0, 50, (h, w))
-        zitter = np.zeros_like(image)
-        zitter[:, :, 1] = noise
-        noise_image = cv2.add(image, zitter)
-        noise_images.append(noise_image)
+'''return list'''
+# def ColorJittering(image, numbers):
+#     h, w, c = image.shape
+#     noise_images = []
+#     for i in range(numbers):
+#         noise = np.random.randint(0, 50, (h, w))
+#         zitter = np.zeros_like(image)
+#         zitter[:, :, 1] = noise
+#         noise_image = cv2.add(image, zitter)
+#         noise_images.append(noise_image)
     
-    return noise_images
+#     return noise_images
+
+'''return only one'''
+def ColorJittering(image):
+    h, w, c = image.shape
+    noise = np.random.randint(0, 50, (h, w))
+    zitter = np.zeros_like(image)
+    zitter[:, :, 1] = noise
+    noise_image = cv2.add(image, zitter)
+    return noise_image
 
 def EdgeEnhance(image):
     Edge_image = cv2.detailEnhance(image, sigma_s=10, sigma_r=0.15)
     return Edge_image
 
-def Brightness(image, numbers):
-    brightnessed_images = []
-    for i in range(numbers):
-        brightnessed_image = tf.image.random_brightness(image, 0.5, seed=12345)
-        brightnessed_images.append(brightnessed_image.eval())
-    return brightnessed_images
+'''return list'''
+# def Brightness(image, numbers):
+#     brightnessed_images = []
+#     for i in range(numbers):
+#         brightnessed_image = tf.image.random_brightness(image, 0.5, seed=12345)
+#         brightnessed_images.append(brightnessed_image.eval())
+#     return brightnessed_images
 
+'''return only one'''
+def Brightness(image):
+    brightnessed_image = tf.image.random_brightness(image, 0.5, seed=12345)
+    return brightnessed_image.eval()
 
 if __name__ == '__main__':
     car_data = pd.read_csv('./CNN/car_information.csv')
-    data_augmenttation(DIR, car_data)
+    data_augmenttation(DIR, car_data, SAVE)
