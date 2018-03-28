5b87c01c9da485dffcac0cb68c1116478fbef369
starsingchow <starstarstarchow@gmail.com>
2018-03-28 16:46:51 +0800

    .

--- input_data.py
+++ input_data.py
@@ -76,7 +76,7 @@ def get_data(dir,batch_size):
 
     dataset = tf.data.Dataset.from_tensor_slices((filenames, labels_list))
     dataset = dataset.map(_parse_function)
-    dataset = dataset.shuffle(buffer_size=1000).repeat().batch(100)
+    dataset = dataset.shuffle(buffer_size=1000).repeat().batch(1000)
 
     iterator = dataset.make_one_shot_iterator()
     # images, labels = iterator.get_next()
