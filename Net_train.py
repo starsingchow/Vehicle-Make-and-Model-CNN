a64d2345b52364bc93d7b27d3a5b491e42f15da8
starsingchow <starstarstarchow@gmail.com>
2018-03-28 20:43:31 +0800

    .

--- Net_train.py
+++ Net_train.py
@@ -145,14 +145,14 @@ def train(net, net_para, label, keep_prob, save_dir):
 
     # cross_entropy_mean = tf.reduce_mean(cross_entropy)
     logits =  y+1e10
-    cross_entropy_mean = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_))
+    cross_entropy_mean = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y_)) / BATCH_SIZE
     with tf.name_scope('loss'):
         loss = cross_entropy_mean
         tf.summary.scalar('loss', loss)
 
     with tf.name_scope('accuracy'):
         correct_prediction = tf.equal(tf.argmax(y,axis=1),y_)
-        correct_rate = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
+        correct_rate = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         tf.summary.scalar('accuracy', correct_rate)
 
     learning_rate = tf.train.exponential_decay(
