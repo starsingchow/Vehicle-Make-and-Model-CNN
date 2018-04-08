bd3f543f8e2297d9c4eb7ddc503f587516b0ab19
starsingchow <starstarstarchow@gmail.com>
2018-04-08 15:43:08 +0800

    .

--- Net_train_V3.py
+++ Net_train_V3.py
@@ -144,7 +144,7 @@ MOVING_AVERAGE_DECAY = 0.99
 def train(net, net_para, label, keep_prob, save_dir, log_dir):
     times_1000 = net_para.train_steps/1000
     summary_writer = tf.summary.FileWriter(log_dir)
-    for t in range(25,int(times_1000)):
+    for t in range(35,int(times_1000)):
         tf.reset_default_graph()
         graph = tf.Graph()
         with graph.as_default() as g:
